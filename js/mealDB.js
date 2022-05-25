5728f4e21b6ec1d988a1d93b7d2c97d4d082508c
Shafayathub <shafayat.ms@gamilcom>
2022-05-25 20:37:15 +0600

    meals are showing

--- js/mealDB.js
+++ js/mealDB.js
@@ -7,5 +7,29 @@ const searchFood = () => {
   //   console.log(url);
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => displaySearchResults(data.meals));
+};
+const displaySearchResults = (meals) => {
+  const searchResult = document.getElementById('search-result');
+  meals.forEach((meal) => {
+    console.log(meal);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions}
+                    </p>
+                </div>
+                <div class="card-footer">
+                   <small class="text-muted">
+                      <a target=_blank href="${meal.strYoutube}" class="btn btn-primary stretched-link">Watch it over Youtube</a>
+                   </small>
+                </div>
+            </div>
+    `;
+    searchResult.appendChild(div);
+  });
 };
