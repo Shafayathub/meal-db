58752bb8f181b59b06c0e584b93f3ddec0c5fa27
Shafayathub <shafayat.ms@gamilcom>
2022-05-25 21:42:33 +0600

    site done

--- js/mealDB.js
+++ js/mealDB.js
@@ -1,21 +1,50 @@
 const searchFood = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
-  //   console.log(searchText);
+  // clear search box
   searchField.value = '';
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-  //   console.log(url);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => displaySearchResults(data.meals));
+  if (searchText == '') {
+    alert('Please enter text to search');
+  } else {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    // load data
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => displaySearchResults(data.meals));
+  }
 };
 const displaySearchResults = (meals) => {
+  console.log(meals);
   const searchResult = document.getElementById('search-result');
-  meals.forEach((meal) => {
-    console.log(meal);
+  // clear loaded content
+  searchResult.textContent = '';
+  if (meals == null) {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
+      <div class="card">
+        <div class="card-header">
+          Quote
+        </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+            <p>We don't have such food, please search another item or stay hungry.</p>
+            <footer class="blockquote-footer">Another Eater <cite title="Source Title">MD shafayat Islam</cite></footer>
+            </blockquote>
+          </div>
+          <div class="card-footer">
+                   <small class="text-muted">
+                      <a target=_blank href="https://www.linkedin.com/in/md-shafayat-islam-97000110b/" class="btn btn-warning stretched-link">Meet the eater</a>
+                   </small>
+          </div>
+      </div>`;
+    searchResult.appendChild(div);
+  } else {
+    meals.forEach((meal) => {
+      console.log(meal);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
             <div class="card h-100">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
                 <div class="card-body">
@@ -25,11 +54,12 @@ const displaySearchResults = (meals) => {
                 </div>
                 <div class="card-footer">
                    <small class="text-muted">
-                      <a target=_blank href="${meal.strYoutube}" class="btn btn-primary stretched-link">Watch it over Youtube</a>
+                      <a target=_blank href="${meal.strYoutube}" class="btn btn-warning stretched-link">Watch it over Youtube</a>
                    </small>
                 </div>
             </div>
-    `;
-    searchResult.appendChild(div);
-  });
+      `;
+      searchResult.appendChild(div);
+    });
+  }
 };
